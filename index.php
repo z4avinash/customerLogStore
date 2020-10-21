a60fce5df6498f4bece815b3edeca39bc4ad2a74
z4avinash <avinashphp.aipl@gmail.com>
2020-10-21 18:56:11 +0530

    initial files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CustomerLog | STORE</title>
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <h1 style="text-align: center;">Customer Log Store</h1>
+    <hr><br><br>
+    <!-- Script -->
+    <script src="script.js"></script>
+</body>
+
+</html>
